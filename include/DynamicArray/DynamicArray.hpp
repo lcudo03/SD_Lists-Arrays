62ce59d74a01db406b9b0a0eb83815821e537898
Lukasz <cudo447@gmail.com>
2025-04-05 17:09:55 +0200

    Implementacja i deklaracja funkcji w DynamicArray.hpp
    -deklaracja funkcji
    -dodanie funkcji wstawiających: w losowe miejsce, ostatnie oraz pierwsze
    -dodanie funkcji rozszerzającej tablice

--- include/DynamicArray/DynamicArray.hpp
+++ include/DynamicArray/DynamicArray.hpp
@@ -7,9 +7,20 @@ private:
     T* array;
     int size;
     int currentSize;
+
+    void resize();
 public:
     DynamicArray();
     ~DynamicArray();
+
+    void insertAtStart(T value);
+    void insertAtEnd(T value)
+    void insertAtRandom(T value, int index);
+    void removeFromStart();
+    void removeFromEnd();
+    void removeAtRandom(int index);
+    bool search(T value);
+    void print();
 };
 
 template <typename T>
@@ -22,3 +33,32 @@ DynamicArray<T>::~DynamicArray()
 {
     delete[] array
 }
+
+template <typename T>
+void DynamicArray<T>::resize(){
+    size *= 2;
+    T *newArray = new T[size];
+    for (int i = 0; i < currentSize; i++)
+    {
+        newArray[i] = array[i];
+    }
+    delete array;
+    array = newArray;
+}
+
+template <typename T>
+void DynamicArray<T>::insertAtStart(T value){
+    if(currentSize == size) resize();
+    for(int i = currentSize; i > 0; i--) array[i] = array[i-1];
+    array[0] = value;
+    currentSize++;
+}
+
+template <typename T>
+void DynamicArray<T>::insertAtEnd(T value){
+    if(currentSize == capacity) resize();
+    array[currentSize++] = value;
+}
+
+template <typename T>
+void DynamicArray<T>::insertAtRandom(T value)
